709bc640c147d5c5ff1bbacc6b1120ad481a5519
Andre Oliveira <order_yellow@hotmail.com>
2020-11-06 21:37:41 -0300

    Fixed MySQL Connection

--- src/main/java/br/uniso/tarefa/tarefa/CarroRepository.java
+++ src/main/java/br/uniso/tarefa/tarefa/CarroRepository.java
@@ -1,6 +1,8 @@
 package br.uniso.tarefa.tarefa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarroRepository extends JpaRepository<Carro, Integer> {
 }
